c3909c3ca4fc84a2492a6995e9990c1672c11708
noora58 <noorasherief@gmail.com>
2025-07-14 20:49:14 +0530

    projct updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import AddTodo from './components/AddTodo';
+import SearchTodo from './components/SearchTodo';
+import DeleteTodo from './components/DeleteTodo';
 
 function App() {
   return (
     <div>
         <AddTodo/>
+        <SearchTodo/>
+        <DeleteTodo/>
     </div>
   );
 }
