73aba7425d2c2271b58e2662596078c7566e9a43
noora58 <noorasherief@gmail.com>
2025-07-14 20:39:43 +0530

    project created

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const AddTodo = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <label htmlFor="" className="form-label">user_id</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <label htmlFor="" className="form-label">id</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <label htmlFor="" className="form-label">title</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <label htmlFor="" className="form-label">completed</label><br />
+                            <input type="checkbox" name="true" id="" className="form-check-input" />true <br />
+                            <input type="checkbox" name="false" id="" className="form-check-input" />false
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <button className="btn btn-success">Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddTodo
